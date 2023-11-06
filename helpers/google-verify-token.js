c081d814d8ddb7b8e3f810c04853d3ef5dc5640f
Chaps <sarchionicesar@gamil.com>
2023-11-06 08:26:05 -0300

    Actualizacion 1.2

--- helpers/google-verify-token.js
+++ helpers/google-verify-token.js
@@ -7,13 +7,16 @@ const client = new OAuth2Client(CLIENT_ID);
 
 const validarGoogleId = async (token) => {
   try {
+    console.log('========= Token ==================')
+    console.log(token)
 
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: [
         CLIENT_ID,          
         '378432942091-komg6303ioivtqad4q94q7c2glhi9g0p.apps.googleusercontent.com',
-        '378432942091-8t4oqibru97jmj1n2jmstji8bd4vqede.apps.googleusercontent.com'
+        '378432942091-8t4oqibru97jmj1n2jmstji8bd4vqede.apps.googleusercontent.com',
+        
       ],  
       // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
